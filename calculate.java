8bd4c7180f2c82c84e3f0be41b85c3a5e2770e0f
Sayli1915 <109417390+Sayli1915@users.noreply.github.com>
2022-07-26 13:32:17 +0530

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author shree
+ */
+import java.util.*;
+public class calculate {
+    public static void main(String args [])
+    {
+        int m,n,opt,add,sub,mul;
+        double div;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        m= s.nextInt();
+        System.out.println("Enter second value:");
+        n= s.nextInt();
+        while(true)
+        {
+            System.out.println("Enter 1 for addition");
+            System.out.println("")
+        }}
+}
